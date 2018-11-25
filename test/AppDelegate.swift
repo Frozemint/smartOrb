1be09ebf40897b21d2d89fb0de27a732e314cd6a
Nami <nami5504@gmail.com>
2018-11-24 22:09:16 -0800

    Initial Commit

---
+++ test/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by Nami on 2018-11-24.
+//  Copyright © 2018 Nami. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
