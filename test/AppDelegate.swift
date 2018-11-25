ed2c104a586ea8316bbad246253dcc60013244ef
Nami <nami5504@gmail.com>
2018-11-25 11:15:04 -0800

    Initial commit

--- test/AppDelegate.swift
+++ test/AppDelegate.swift
@@ -11,17 +11,90 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+    
+    
+    @IBOutlet var appStatusMenu: NSMenu!
+    @IBAction func statusBarLightOn(_ sender: Any){
+        switchLightOn()
+    }
+    
+    @IBAction func statusBarLightOff(_ sender: Any){
+        switchLightOff()
+    }
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    struct Custom: Codable{
+        let power: String?
+        let brightness: Float?
+        let duration: Float?
+        let color: String?
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        systemWakeSleepNotification()
+        statusItem.button?.title = "💡"
+        statusItem.menu = appStatusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        switchLightOff()
+    }
+    
+    func systemWakeSleepNotification(){
+        //This function makes it so that when the macbook go to sleep or wakes from sleep
+        //(closing or opening lid) gives us a notification
+        //which in turns calls
+        NSWorkspace.shared.notificationCenter.addObserver(self,
+                                                          selector: #selector(switchLightOff),
+                                                          name: NSWorkspace.willSleepNotification,
+                                                          object: nil)
+        NSWorkspace.shared.notificationCenter.addObserver(self,
+                                                          selector: #selector(switchLightOn),
+                                                          name: NSWorkspace.didWakeNotification,
+                                                          object: nil)
+    }
+    
+    @objc func switchLightOff(){
+        toggleOnOffOfLight(power: "off")
+        print("System sleeping, turning light off")
+    }
+    @objc func switchLightOn(){
+        toggleOnOffOfLight(power: "on")
+        print("System waking, turnig light on")
     }
 
+    func toggleOnOffOfLight(power: String){
+        let endpoint = URL(string: "https://api.lifx.com/v1/lights/all/state")
+        var urlRequest = URLRequest(url: endpoint!)
+        urlRequest.httpMethod = "PUT"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.setValue("Bearer \(apiToken)", forHTTPHeaderField: "Authorization")
+        let parameters = Custom(power: power, brightness: nil, duration: nil, color: nil)
+        guard let payload = try? JSONEncoder().encode(parameters) else {
+            print("Error")
+            return
+        }
+        let task = URLSession.shared.uploadTask(with: urlRequest, from: payload) { data, response, error in
+            if error != nil {
+                print ("error: \(error!)")
+                return
+            }
+            let response = response as! HTTPURLResponse
+            if !(200...299).contains(response.statusCode) {
+                print("Server error: server code \(response.statusCode)")
+            }
+            if let mimeType = response.mimeType,
+                mimeType == "application/json",
+                let data = data,
+                let dataString = String(data: data, encoding: .utf8) {
+                print ("Got Data from server: \(dataString)")
+            }
+        }
+        task.resume() //perform the upload task
+    }
 
 }
 
